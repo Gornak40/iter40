b5402a4352f00f8a4932db05da08648fa608a248
Gornak40 <s-kozelsk@yandex.ru>
2022-05-11 09:49:24 +0300

    final compilation with non labels is working (is it?)

--- iter40.py
+++ iter40.py
@@ -7,21 +7,26 @@ from token_types import *
 from os import system
 
 
-@command(help='ITER40 compiler made by Gornak40.')
+@command(help='ITER compiler made by Gornak40.')
 @argument('source', type=File())
 @option('--outname', '-o', default='main', type=Path(writable=True, dir_okay=False), help='Output file name.')
 class Main:
-	tokens = []
-	assign = []
-	bss = []
-	text = []
+	tokens = list()
+	assign = list()
+	bss = list()
+	text = list()
+	ptr = 0
+	label = 0
+	ban = set()
 
 	def __init__(self, source, outname):
 		self.lex(source)
 		self.check_balance()
+		self.set_include()
 		self.set_assign()
 		self.set_bss()
-
+		self.text = list(self.set_text())
+		pprint(self.tokens)
 		self.build(outname)
 
 	def build(self, outname):
@@ -55,24 +60,50 @@ class Main:
 		if balance:
 			balance_error()
 
+	# TODO
+	def set_include(self):
+		pass
+
 	def set_assign(self):
-		for ptoken, token in zip(self.tokens, self.tokens[1:]):
+		for i, (ptoken, token) in enumerate(zip(self.tokens, self.tokens[1:])):
 			if token.gettokentype() == 'SETCONST':
 				if ptoken.gettokentype() not in CONSTVAL:
 					const_error(ptoken)
 				self.assign.append(f'%assign {token.getstr()[1:]} {ptoken.getstr()}')
-				ptoken = token = None
+				self.ban |= {i, i + 1}
 
 	def set_bss(self):
-		for ptoken, token in zip(self.tokens, self.tokens[1:]):
+		for i, (ptoken, token) in enumerate(zip(self.tokens, self.tokens[1:])):
 			if token.gettokentype() in BSS:
 				self.bss.append(f'{token.getstr()[1:]}: resd 1')
 			elif token.gettokentype() == 'STATARR':
 				if ptoken.gettokentype() not in CONSTVAL:
 					const_error(ptoken)
 				self.bss.append(f'{token.getstr()[1:]}: resd {ptoken.getstr()}')
-				ptoken = token = None
+				self.ban |= {i, i + 1}
 		self.bss = list(set(self.bss))
+
+	def set_text(self):
+		while self.ptr < len(self.tokens):
+			token = self.tokens[self.ptr]
+			if self.ptr in self.ban or token.gettokentype() == 'COMMENT':
+				self.ptr += 1
+			elif token.gettokentype() in COND:
+				# TODO
+				self.ptr += 1
+			elif token.gettokentype() == 'ITER':
+				# TODO
+				self.ptr += 1
+			elif token.gettokentype() == 'SETFUNC':
+				# TODO
+				self.ptr += 1
+			elif token.gettokentype() in PARAM:
+				value = str().join(filter(lambda x: x in VAR, token.getstr()))
+				yield f'@{token.gettokentype().lower()} {value}'
+				self.ptr += 1
+			else:
+				yield f'@{token.gettokentype().lower()}'
+				self.ptr += 1
 
 
 if __name__ == '__main__':
--- token_types.py
+++ token_types.py
@@ -1,3 +1,9 @@
-OPEN = {'JNE0', 'JE0', 'JL', 'JLE', 'JG', 'JGE', 'JE', 'JNE', 'ELSE', 'ITER', 'SETFUNC'}
+from string import ascii_letters, digits
+
+
+VAR = set(ascii_letters) | set(digits) | {'_'}
+COND = {'JNE0', 'JE0', 'JL', 'JLE', 'JG', 'JGE', 'JE', 'JNE'}
+OPEN = COND | {'ELSE', 'ITER', 'SETFUNC'}
 CONSTVAL = {'GETCONST', 'NUM'}
 BSS = {'SETVAR', 'HEAPARR'}
+PARAM = {'NUM', 'GETVAR', 'GETCONST', 'GETARR', 'SETVAR', 'LSETARR', 'RSETARR', 'HEAPARR'}
